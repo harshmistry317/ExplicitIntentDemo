7efc419e4fecad431314147bcfb4d09ed354deea
harsh317 <mistryhk317@gmail.com>
2022-12-29 15:19:45 +0530

    1st activity kt file ready and 2nd activity file add

--- app/src/main/java/com/harsh_mistry/explicitintent/MainActivity.kt
+++ app/src/main/java/com/harsh_mistry/explicitintent/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.harsh_mistry.explicitintent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var nextbutton=findViewById<Button>(R.id.btn_next)
+        nextbutton.setOnClickListener {
+            var intent= Intent(applicationContext,MainActivity2::class.java)
+            intent.putExtra("username","Harsh")
+            startActivity(intent)
+        }
+
     }
 }
