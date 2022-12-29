7cbead170dd6f626bf6dc4ef02b47e18cb8927d2
harsh317 <mistryhk317@gmail.com>
2022-12-29 15:22:31 +0530

    Explicit Intent

--- app/src/main/java/com/harsh_mistry/explicitintent/MainActivity2.kt
+++ app/src/main/java/com/harsh_mistry/explicitintent/MainActivity2.kt
@@ -2,10 +2,14 @@ package com.harsh_mistry.explicitintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        var intent=getIntent()
+        var unm=intent.getStringExtra("username")
+        Toast.makeText(applicationContext,"Hello $unm", Toast.LENGTH_LONG).show()
     }
 }
